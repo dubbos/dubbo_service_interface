b97562f9eacd332b0653f301dbfb76765823f421
李亚军 <liyj@lifengyun.com>
2017-07-13 17:26:34 +0800

    dubbo_service_interface dubbo接口

---
+++ src/main/java/com/dubbo/test/inter/service/TestService.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) Windliven 2016 All Rights Reserved
+ *
+ * @author liyj
+ * @date 2017年7月10日 上午10:09:53
+ * @since V1.0.0
+ */
+package com.dubbo.test.inter.service;
+
+/**
+ * TODO
+ *
+ * @author liyj
+ * @date 2017年7月10日 上午10:09:53
+ *
+ */
+public interface TestService {
+    
+    public String getName(String name);
+}
